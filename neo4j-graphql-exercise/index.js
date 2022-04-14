0119d953a2cc666746274bb78e14497ef59f812b
Matthijs Liethof <matthijsliethof@protonmail.com>
2022-04-14 17:07:53 +0200

    Implement start of todo based exercises

--- neo4j-graphql-exercise/index.js
+++ neo4j-graphql-exercise/index.js
@@ -1,21 +1,36 @@
 const { Neo4jGraphQL } = require("@neo4j/graphql");
-const { toGraphQLTypeDefs } = require("@neo4j/introspector");
 const neo4j = require("neo4j-driver");
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server");
 
 const driver = neo4j.driver(
     "neo4j://localhost:7687",
     neo4j.auth.basic("neo4j", "s3cr3t")
 );
 
-const sessionFactory = () => driver.session({ defaultAccessMode: neo4j.session.READ })
+const typeDefs = gql`
+    type Todo {
+        status: Boolean
+        description: String
+        completedBy: Person @relationship(type: "COMPLETED_BY", direction: IN, properties: "CompletedBy")
+        belongsToProject: Project! @relationship(type: "BELONGS_TO", direction: OUT)
+    }
+    type Project {
+        name: String
+        todos: [Todo!]! @relationship(type:"BELONGS_TO", direction: IN)
+    }
+    type Person {
+        name: String
+        completedTodos: [Todo!]! @relationship(type:"COMPLETED_BY", properties: "CompletedBy", direction: OUT)
+    }
+
+    interface CompletedBy @relationshipProperties {
+        completedOn: DateTime!
+    }
+`
 
 // We create a async function here until "top level await" has landed
 // so we can use async/await
 async function main() {
-    const readonly = true; // We don't want to expose mutations in this case
-    const typeDefs = await toGraphQLTypeDefs(sessionFactory, readonly)
-
     const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
 
     const server = new ApolloServer({
